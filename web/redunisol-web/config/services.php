19a05d69ed81bc79170b04bbd5f0f1b05b2ae2b5
nsallitto <nicosallitto@gmail.com>
2026-04-27 12:51:33 -0300

    Increase Redunisol Web timeout for Kestra form webhook

--- web/redunisol-web/config/services.php
+++ web/redunisol-web/config/services.php
@@ -43,7 +43,7 @@ return [
 
     'kestra' => [
         'form_webhook_url' => env('KESTRA_FORM_WEBHOOK_URL'),
-        'form_webhook_timeout_seconds' => (int) env('KESTRA_FORM_WEBHOOK_TIMEOUT_SECONDS', 15),
+        'form_webhook_timeout_seconds' => (int) env('KESTRA_FORM_WEBHOOK_TIMEOUT_SECONDS', 60),
         'default_lead_source' => env('KESTRA_FORM_DEFAULT_LEAD_SOURCE', 'Google'),
     ],
 
